9b0364e27fd39d576dac1ba6a9dbaac150e67320
sem-ihor <igorsmnt@gmail.com>
2022-11-03 23:39:44 +0200

    For check user

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ bot.on('message', async (msg) => {
 
     if (text === '/start') {
         await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/f40/774/f4077484-5680-377b-a6fc-c9fd165063ff/6.webp')
-        await bot.sendMessage(chatId, `Welcome the owner of the wonderful Opel!`)
+        return await bot.sendMessage(chatId, `Welcome the owner of the wonderful Opel!`)
     }
     if (text === '/info') {
-        await bot.sendMessage(chatId, `I know you are ${msg.from.first_name} and you love your Opel Omega 1998...`)
+        return await bot.sendMessage(chatId, `I know you are ${msg.from.first_name} and you love your Opel Omega 1998...`)
     }
 
-
+    return await bot.sendMessage(chatId, `Huy tobi v sraku`)
 })
